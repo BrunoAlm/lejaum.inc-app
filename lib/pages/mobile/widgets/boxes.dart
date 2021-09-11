ea59f4e6b2f328145313e4e18f512fb50324e500
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2021-09-11 11:00:02 -0300

    text boxes update

--- lib/pages/mobile/widgets/boxes.dart
+++ lib/pages/mobile/widgets/boxes.dart
@@ -1,4 +1,56 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lejaum/pages/services/styles.dart';
+
+class BoxSino extends StatelessWidget {
+  final Color quaseWhite = Color(0xFFF4F4F4);
+  final Color laranjaum = Color(0xFFFF5400);
+  BoxSino({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width * 0.9,
+            decoration: BoxDecoration(
+              color: quaseWhite,
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(width: 2, color: laranjaum),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(8, 7, 9, 7),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/sino_icon.png'),
+                  const SizedBox(width: 8),
+                  RichText(
+                    text: TextSpan(
+                      style: Styles.boxesStyle,
+                      children: [
+                        TextSpan(text: 'Apareça no digital para a\n'),
+                        TextSpan(text: 'pessoa certa e no momento\n'),
+                        TextSpan(text: 'certo com o '),
+                        TextSpan(
+                            text: 'tráfego pago.',
+                            style: Styles.boxesStyleBold),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
 class BoxMao extends StatelessWidget {
   final String font = 'Georama';
@@ -28,62 +80,18 @@ class BoxMao extends StatelessWidget {
                 children: [
                   Image.asset('assets/images/mao_icon.png'),
                   const SizedBox(width: 8),
-                  Text(
-                    'Seja reconhecido pelo seus\nclientese fidelize eles com a\nidentidade visual.',
-                    style: TextStyle(
-                      color: laranjaum,
-                      fontFamily: font,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 17,
+                  RichText(
+                    text: TextSpan(
+                      style: Styles.boxesStyle,
+                      children: [
+                        TextSpan(text: 'Seja reconhecido pelos seus\n'),
+                        TextSpan(text: 'clientes e fidelize eles com a\n'),
+                        TextSpan(
+                            text: 'identidade visual.',
+                            style: Styles.boxesStyleBold),
+                      ],
                     ),
-                  )
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class BoxSino extends StatelessWidget {
-  final String font = 'Georama';
-  final Color laranjaum = Color(0xFFFF5400);
-  final Color quaseWhite = Color(0xFFF4F4F4);
-  BoxSino({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(bottom: 10.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width * 0.9,
-            decoration: BoxDecoration(
-              color: quaseWhite,
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(width: 2, color: laranjaum),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(8, 7, 9, 7),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset('assets/images/sino_icon.png'),
-                  const SizedBox(width: 8),
-                  Text(
-                    'Apareça no digital para a\npessoa certa e no momento\ncerto.',
-                    style: TextStyle(
-                      color: laranjaum,
-                      fontFamily: font,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 17,
-                    ),
-                  )
+                  ),
                 ],
               ),
             ),
@@ -122,15 +130,17 @@ class BoxGrafico extends StatelessWidget {
                 children: [
                   Image.asset('assets/images/grafico_icon.png'),
                   const SizedBox(width: 8),
-                  Text(
-                    'Transformamos Leads\n em Vendas com o marketing',
-                    style: TextStyle(
-                      color: laranjaum,
-                      fontFamily: font,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 17,
+                  RichText(
+                    text: TextSpan(
+                      style: Styles.boxesStyle,
+                      children: [
+                        TextSpan(text: 'Transformamos Leads\n'),
+                        TextSpan(text: 'em Vendas com o '),
+                        TextSpan(
+                            text: 'marketing.', style: Styles.boxesStyleBold),
+                      ],
                     ),
-                  )
+                  ),
                 ],
               ),
             ),
---
+++ lib/pages/services/styles.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Styles {
+  // box text style
+  static const boxesStyle = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 17,
+    color: Color(0xFFFF5400),
+  );
+  // extends box text style to bold
+  static const boxesStyleBold = TextStyle(
+    fontWeight: FontWeight.w900,
+    fontSize: 17,
+    color: Color(0xFFFF5400),
+  );
+}
