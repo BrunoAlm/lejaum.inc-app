3fef0699aa8818cc784a473dd8fd3026505bf272
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2021-09-11 02:02:47 -0300

    laranjaum ++

--- lib/pages/home/widgets/AppBar/web_app_bar.dart
+++ lib/pages/home/widgets/AppBar/web_app_bar.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class WebAppBar extends StatelessWidget {
-  const WebAppBar({Key? key}) : super(key: key);
+  final Color laranjaum = Color(0xFFFF5400);
+  WebAppBar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      backgroundColor: Color(0xFFFF5400),
+      systemOverlayStyle: SystemUiOverlayStyle(statusBarColor: laranjaum),
+      backgroundColor: laranjaum,
       toolbarHeight: 72,
       elevation: 5,
       title: Row(
         children: [
+          SvgPicture.asset("assets/images/logo_icon.svg"),
+          const SizedBox(width: 3),
           Text(
             'lejaum',
             style: TextStyle(
