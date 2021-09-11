f9fab4276d7cba5551ab1a8e759d32022626e471
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2021-09-11 01:27:25 -0300

    padding da esquerda do trem arrumado

--- lib/pages/home/widgets/AppBar/mobile_app_bar.dart
+++ lib/pages/home/widgets/AppBar/mobile_app_bar.dart
@@ -7,23 +7,20 @@ class MobileAppBar extends StatelessWidget {
     return AppBar(
       iconTheme: IconThemeData(),
       backgroundColor: Color(0xFFFF5400),
-      title: Padding(
-        padding: const EdgeInsets.only(left: 20),
-        child: Row(
-          children: [
-            Image.asset("assets/images/logo_icon.png"),
-            const SizedBox(width: 3),
-            Text(
-              'lejaum',
-              style: TextStyle(
-                color: Color(0xFFE5E5E5),
-                fontFamily: 'Georama',
-                fontStyle: FontStyle.italic,
-                fontSize: 30,
-              ),
+      title: Row(
+        children: [
+          Image.asset("assets/images/logo_icon.png"),
+          const SizedBox(width: 3),
+          Text(
+            'lejaum',
+            style: TextStyle(
+              color: Color(0xFFE5E5E5),
+              fontFamily: 'Georama',
+              fontStyle: FontStyle.italic,
+              fontSize: 30,
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
