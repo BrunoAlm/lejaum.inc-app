d4fc945057ec4a32689d4d333c64ac178925b42d
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2022-01-19 14:18:04 -0300

    Update galeria_content.dart

--- lib/pages/mobile/widgets/tela3-4-5/widgets/galeria_content.dart
+++ lib/pages/mobile/widgets/tela3-4-5/widgets/galeria_content.dart
@@ -9,47 +9,34 @@ class SocialMedia1 extends StatelessWidget {
   Widget build(BuildContext context) {
     var _altura = 37.h;
     var _largura = _altura / 1.764;
-    // var _altura =
-    //     responsive.ResponsiveValue(context, defaultValue: 251.35, valueWhen: [
-    //   responsive.Condition.equals(
-    //       name: responsive.MOBILE, value: 251.35 * 0.85),
-    //   responsive.Condition.equals(name: 'MOBILE2', value: 251.35 * 1.2),
-    //   responsive.Condition.equals(name: 'AIPEDE', value: 251.35 * 1.5),
-    //   // responsive.Condition.equals(name: responsive.DESKTOP, value: 60.0)
-    // ]).value;
-    // var _largura =
-    //     responsive.ResponsiveValue(context, defaultValue: 142.43, valueWhen: [
-    //   responsive.Condition.equals(
-    //       name: responsive.MOBILE, value: 142.43 * 0.85),
-    //   responsive.Condition.equals(name: 'MOBILE2', value: 142.43 * 1.2),
-    //   responsive.Condition.equals(name: 'AIPEDE', value: 142.43 * 1.5),
-    //   // responsive.Condition.equals(name: responsive.DESKTOP, value: 60.0)
-    // ]).value;
 
-    return GestureDetector(
-      onTap: () {
-        // Navigator.push(context, MaterialPageRoute(builder: (context) {
-        //   return const FlorDoGraoZoom();
-        // }));
-      },
-      child: Hero(
-        tag: 'florDoGrao',
-        child: Padding(
-          padding: const EdgeInsets.only(right: 10),
-          child: Container(
+    return Padding(
+      padding: const EdgeInsets.only(right: 10),
+      child: Container(
+        height: _altura,
+        width: MediaQuery.of(context).size.width,
+        margin: const EdgeInsets.only(bottom: 10),
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          physics: const BouncingScrollPhysics(), // this for snapping
+          itemCount: social_media1_images.length,
+          itemBuilder: (_, index) => Container(
+            width: _largura,
             height: _altura,
-            width: MediaQuery.of(context).size.width,
-            margin: const EdgeInsets.only(bottom: 10),
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              physics: const BouncingScrollPhysics(), // this for snapping
-              itemCount: social_media1_images.length,
-              itemBuilder: (_, index) => Container(
-                width: _largura,
-                height: _altura,
-                margin: EdgeInsets.only(left: 10),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
+            margin: EdgeInsets.only(left: 10),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) {
+                      return SocialMedia1Zoom(index: index);
+                    }),
+                  );
+                },
+                child: Hero(
+                  tag: 'sm1_hero',
                   child: Image.asset(
                     social_media1_images[index],
                     fit: BoxFit.fill,
@@ -64,34 +51,27 @@ class SocialMedia1 extends StatelessWidget {
   }
 }
 
-// class FlorDoGraoZoom extends StatelessWidget {
-//   const FlorDoGraoZoom({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     var largura = MediaQuery.of(context).size.width;
-//     return GestureDetector(
-//       onTap: () {
-//         Navigator.pop(context);
-//       },
-//       child: Center(
-//         child: Hero(
-//           tag: 'florDoGrao',
-//           child: Container(
-//             height: 500,
-//             width: largura,
-//             margin: const EdgeInsets.only(bottom: 10),
-//             child: ListView.builder(
-//                 scrollDirection: Axis.horizontal,
-//                 physics: const BouncingScrollPhysics(), // this for snapping
-//                 itemCount: flor_do_grao_zoom_list.length,
-//                 itemBuilder: (_, index) => flor_do_grao_zoom_list[index]),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+class SocialMedia1Zoom extends StatelessWidget {
+  const SocialMedia1Zoom({Key? key, required this.index}) : super(key: key);
+  final int index;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => Navigator.pop(context),
+      child: Center(
+        child: Hero(
+          tag: 'sm1_hero',
+          child: Container(
+            child: Image.asset(
+              social_media1_images[index],
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
 
 class SocialMedia2 extends StatelessWidget {
   const SocialMedia2({Key? key}) : super(key: key);
@@ -100,21 +80,6 @@ class SocialMedia2 extends StatelessWidget {
   Widget build(BuildContext context) {
     var _altura = 37.h;
     var _largura = _altura / 1.0041;
-    // var _altura =
-    //     responsive.ResponsiveValue(context, defaultValue: 244.92, valueWhen: [
-    //   responsive.Condition.equals(
-    //       name: responsive.MOBILE, value: 244.92 * 0.85),
-    //   responsive.Condition.equals(name: 'MOBILE2', value: 244.92 * 1.2),
-    //   responsive.Condition.equals(name: 'AIPEDE', value: 244.92 * 1.5),
-    //   // responsive.Condition.equals(name: responsive.DESKTOP, value: 60.0)
-    // ]).value;
-    // var _largura =
-    //     responsive.ResponsiveValue(context, defaultValue: 243.9, valueWhen: [
-    //   responsive.Condition.equals(name: responsive.MOBILE, value: 243.9 * 0.85),
-    //   responsive.Condition.equals(name: 'MOBILE2', value: 243.9 * 1.2),
-    //   responsive.Condition.equals(name: 'AIPEDE', value: 243.9 * 1.5),
-    //   // responsive.Condition.equals(name: responsive.DESKTOP, value: 60.0)
-    // ]).value;
     return Padding(
       padding: const EdgeInsets.only(right: 10),
       child: SizedBox(
@@ -130,7 +95,42 @@ class SocialMedia2 extends StatelessWidget {
             margin: EdgeInsets.only(left: 10),
             child: ClipRRect(
               borderRadius: BorderRadius.circular(10),
-              child: Image.asset(social_media2_images[index], fit: BoxFit.fill),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) {
+                      return SocialMedia2Zoom(index: index);
+                    }),
+                  );
+                },
+                child: Hero(
+                    tag: 'sm2_hero',
+                    child: Image.asset(social_media2_images[index],
+                        fit: BoxFit.fill)),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SocialMedia2Zoom extends StatelessWidget {
+  const SocialMedia2Zoom({Key? key, required this.index}) : super(key: key);
+  final int index;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => Navigator.pop(context),
+      child: Center(
+        child: Hero(
+          tag: 'sm2_hero',
+          child: Container(
+            child: Image.asset(
+              social_media2_images[index],
+              fit: BoxFit.fill,
             ),
           ),
         ),
