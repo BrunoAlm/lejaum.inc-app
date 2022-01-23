61a0e82143f2c94b282891afc3560b7ebfcb778d
BrunoAlm <brunoalmeidadem@gmail.com>
2022-01-22 22:02:14 -0300

    Update galeria_content.dart

--- lib/pages/mobile/widgets/tela3-4-5/widgets/galeria_content/galeria_content.dart
+++ lib/pages/mobile/widgets/tela3-4-5/widgets/galeria_content/galeria_content.dart
@@ -97,7 +97,7 @@ class _SocialMedia1ZoomState extends State<SocialMedia1Zoom> {
               ),
             ),
             Hero(
-              tag: 'sm2_hero_${widget.imagemClicada}',
+              tag: 'sm1_hero_${widget.imagemClicada}',
               child: ZoomOverlay(
                 animationDuration: Duration(milliseconds: 200),
                 minScale: 0.5,
@@ -200,30 +200,7 @@ class SocialMedia2Zoom extends StatefulWidget {
   State<SocialMedia2Zoom> createState() => _SocialMedia2ZoomState();
 }
 
-class _SocialMedia2ZoomState extends State<SocialMedia2Zoom>
-    with SingleTickerProviderStateMixin {
-  late TransformationController controller;
-  late AnimationController animationController;
-  Animation<Matrix4>? animation;
-
-  @override
-  void initState() {
-    controller = TransformationController();
-    animationController = AnimationController(
-      vsync: this,
-      duration: Duration(milliseconds: 200),
-    )..addListener(() => controller.value = animation!.value);
-
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    animationController.dispose();
-    super.dispose();
-  }
-
+class _SocialMedia2ZoomState extends State<SocialMedia2Zoom> {
   @override
   Widget build(BuildContext context) {
     var altura = MediaQuery.of(context).size.height;
@@ -251,11 +228,10 @@ class _SocialMedia2ZoomState extends State<SocialMedia2Zoom>
             ),
             Hero(
               tag: 'sm2_hero_${widget.imagemClicada}',
-              child: InteractiveViewer(
-                transformationController: controller,
-                onInteractionEnd: (details) {
-                  resetAnimation();
-                },
+              child: ZoomOverlay(
+                animationDuration: Duration(milliseconds: 200),
+                minScale: 0.5,
+                maxScale: 3,
                 child: Image.asset(
                   social_media2_images[widget.imagemClicada],
                   fit: BoxFit.fitWidth,
@@ -295,17 +271,6 @@ class _SocialMedia2ZoomState extends State<SocialMedia2Zoom>
         ),
       ),
     );
-  }
-
-  void resetAnimation() {
-    animation = Matrix4Tween(
-      begin: controller.value,
-      end: Matrix4.identity(),
-    ).animate(CurvedAnimation(
-      parent: animationController,
-      curve: Curves.easeIn,
-    ));
-    animationController.forward(from: 0);
   }
 }
 
