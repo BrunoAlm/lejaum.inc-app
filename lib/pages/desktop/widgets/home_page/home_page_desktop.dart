c7b8c3810bdb8d76123556ceaa88cae061ed7b88
BrunoAlm <brunoalmeidadem@gmail.com>
2022-01-25 21:20:54 -0300

    Update home_page_desktop.dart

--- lib/pages/desktop/widgets/home_page/home_page_desktop.dart
+++ lib/pages/desktop/widgets/home_page/home_page_desktop.dart
@@ -52,7 +52,7 @@ class HomePageDesktop extends StatelessWidget {
               color: Styles.roxinho,
               child: Center(
                 child: Text(
-                  'Versão completa do portfólio na versão mobile, pois estamos construindo o nosso site! Pode haver erros ou bugs!',
+                  'Versão completa do portfólio na versão mobile, pois estamos construindo o nosso site! Pode haver partes incompletas!',
                   softWrap: true,
                   style: TextStyle(
                     color: Styles.quaseWhite,
