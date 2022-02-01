705136147e8baa189efb4cb9d1567bacac48fc09
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2022-02-01 09:28:41 -0300

    Update pdf_arzadi.dart

--- lib/pages/mobile/widgets/tela3-4-5/teste_pdf/pdf_arzadi.dart
+++ lib/pages/mobile/widgets/tela3-4-5/teste_pdf/pdf_arzadi.dart
@@ -1,103 +1,38 @@
-// import 'package:flutter/material.dart';
-// import 'package:pdf_render/pdf_render_widgets.dart';
-
-// class PdfArzadi extends StatelessWidget {
-//   const PdfArzadi({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: new AppBar(
-//         title: const Text('Pdf_render example app'),
-//       ),
-//       backgroundColor: Colors.grey,
-//       body: Center(
-//           child: PdfDocumentLoader.openAsset(
-//         'assets/pdf/azardi_shop.pdf',
-//         documentBuilder: (context, pdfDocument, pageCount) => LayoutBuilder(
-//             builder: (context, constraints) => ListView.builder(
-//                 itemCount: pageCount,
-//                 itemBuilder: (context, index) => Container(
-//                     margin: EdgeInsets.all(10),
-//                     padding: EdgeInsets.all(10),
-//                     color: Colors.black12,
-//                     child: PdfPageView(
-//                       pdfDocument: pdfDocument,
-//                       pageNumber: index + 1,
-//                     )))),
-//       )),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+import 'package:lejaum/pages/mobile/services/styles_mobile.dart';
+import 'package:pdf_render/pdf_render_widgets.dart';
 
-class PdfArzadi extends StatefulWidget {
+class PdfArzadi extends StatelessWidget {
   const PdfArzadi({Key? key}) : super(key: key);
-
-  @override
-  _PdfArzadiState createState() => _PdfArzadiState();
-}
-
-class _PdfArzadiState extends State<PdfArzadi> {
-  late PdfViewerController _pdfViewerController;
-
-  @override
-  void initState() {
-    _pdfViewerController = PdfViewerController();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _pdfViewerController;
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text(
-          'Arzadi - Id. Visual e Naming',
-        ),
+        backgroundColor: Styles.pretao,
+        title: const Text('Arzadi - Id. Visual e Naming',
+            style: Styles.textoBrancoBold),
       ),
-      body: Container(
-        child: SfPdfViewer.asset(
+      backgroundColor: Colors.grey,
+      body: Center(
+        child: PdfDocumentLoader.openAsset(
           'assets/pdf/azardi_shop.pdf',
-          controller: _pdfViewerController,
+          documentBuilder: (context, pdfDocument, pageCount) => LayoutBuilder(
+            builder: (context, constraints) => ListView.builder(
+              scrollDirection: Axis.vertical,
+              itemCount: pageCount,
+              itemBuilder: (context, index) => Container(
+                margin: EdgeInsets.all(2),
+                padding: EdgeInsets.all(0),
+                color: Colors.black12,
+                child: PdfPageView(
+                  pdfDocument: pdfDocument,
+                  pageNumber: index + 1,
+                ),
+              ),
+            ),
+          ),
         ),
-      ),
-      floatingActionButton: Row(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton.extended(
-            heroTag: 'previusPage',
-            onPressed: () {
-              _pdfViewerController.previousPage();
-            },
-            elevation: 10,
-            label: Icon(
-              Icons.keyboard_arrow_up,
-              color: Colors.black,
-            ),
-          ),
-          // Expanded(child: Container()),
-          FloatingActionButton.extended(
-            heroTag: 'nextPage',
-            onPressed: () {
-              _pdfViewerController.nextPage();
-            },
-            elevation: 10,
-            label: Icon(
-              Icons.keyboard_arrow_down,
-              color: Colors.black,
-            ),
-          ),
-        ],
       ),
     );
   }
