c8e355f99ccee7cea1c42a53ec39f52552c31659
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2022-02-01 09:02:59 -0300

    teste

--- lib/pages/mobile/widgets/tela3-4-5/teste_pdf/pdf_janfie.dart
+++ lib/pages/mobile/widgets/tela3-4-5/teste_pdf/pdf_janfie.dart
@@ -1,339 +1,41 @@
-// import 'package:flutter/material.dart';
-// import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
-
-// class PdfJanfie extends StatefulWidget {
-//   const PdfJanfie({Key? key}) : super(key: key);
-
-//   @override
-//   _PdfJanfieState createState() => _PdfJanfieState();
-// }
-
-// class _PdfJanfieState extends State<PdfJanfie> {
-//   late PdfViewerController _pdfViewerController;
-
-//   @override
-//   void initState() {
-//     _pdfViewerController = PdfViewerController();
-//     super.initState();
-//   }
-
-//   @override
-//   void dispose() {
-//     _pdfViewerController;
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Colors.black,
-//         title: Text(
-//           'Janfie - Id. Visual e Naming',
-//         ),
-//       ),
-//       body: SfPdfViewer.network(
-//         'https://raw.githubusercontent.com/BrunoAlm/lejaum.inc-app/master/assets/pdf/janfie-apresentacao.pdf',
-//         controller: _pdfViewerController,
-//         onDocumentLoaded: (PdfDocumentLoadedDetails details) {
-//           print(details.document.pages.count);
-//         },
-//       ),
-//       floatingActionButton: Row(
-//         crossAxisAlignment: CrossAxisAlignment.end,
-//         mainAxisAlignment: MainAxisAlignment.end,
-//         children: [
-//           FloatingActionButton.extended(
-//             heroTag: 'previusPage',
-//             onPressed: () {
-//               _pdfViewerController.previousPage();
-//             },
-//             elevation: 10,
-//             label: Icon(
-//               Icons.keyboard_arrow_up,
-//               color: Colors.black,
-//             ),
-//           ),
-//           // Expanded(child: Container()),
-//           FloatingActionButton.extended(
-//             heroTag: 'nextPage',
-//             onPressed: () {
-//               _pdfViewerController.nextPage();
-//             },
-//             elevation: 10,
-//             label: Icon(
-//               Icons.keyboard_arrow_down,
-//               color: Colors.black,
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-// teste + - CARREGA RAPIDO MAS NÃO MOSTRA BEM
-// import 'package:flutter/material.dart';
-// import 'package:lejaum/pages/mobile/services/styles_mobile.dart';
-// import 'package:native_pdf_view/native_pdf_view.dart';
-
-// class PdfJanfie extends StatefulWidget {
-//   @override
-//   _PdfJanfieState createState() => _PdfJanfieState();
-// }
-
-// class _PdfJanfieState extends State<PdfJanfie> {
-//   static final int _initialPage = 1;
-//   int _actualPageNumber = _initialPage, _allPagesCount = 0;
-//   bool isSampleDoc = true;
-//   late PdfController _pdfController;
-
-//   @override
-//   void initState() {
-//     _pdfController = PdfController(
-//       document: PdfDocument.openAsset('assets/pdf/janfie-apresentacao.pdf'),
-//       initialPage: _initialPage,
-//     );
-//     super.initState();
-//   }
-
-//   @override
-//   void dispose() {
-//     _pdfController.dispose();
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) => SafeArea(
-//         child: Scaffold(
-//           appBar: AppBar(
-//             backgroundColor: Styles.pretao,
-//             title: Text(
-//               'Janfie - Id. Visual e Naming',
-//               style: Styles.textoBrancoBold,
-//             ),
-//             actions: <Widget>[
-//               // IconButton(
-//               //   icon: Icon(Icons.navigate_before),
-//               //   onPressed: () {
-//               //     _pdfController.previousPage(
-//               //       curve: Curves.ease,
-//               //       duration: Duration(milliseconds: 100),
-//               //     );
-//               //   },
-//               // ),
-//               Container(
-//                 alignment: Alignment.center,
-//                 child: Text(
-//                   '$_actualPageNumber/$_allPagesCount',
-//                   style: TextStyle(fontSize: 18),
-//                 ),
-//               ),
-//               // IconButton(
-//               //   icon: Icon(Icons.navigate_next),
-//               //   onPressed: () {
-//               //     _pdfController.nextPage(
-//               //       curve: Curves.ease,
-//               //       duration: Duration(milliseconds: 100),
-//               //     );
-//               //   },
-//               // ),
-//               const SizedBox(width: 10),
-//               IconButton(
-//                 icon: Icon(Icons.refresh),
-//                 onPressed: () {
-//                   if (isSampleDoc) {
-//                     _pdfController.loadDocument(PdfDocument.openAsset(
-//                         'assets/pdf/janfie-apresentacao.pdf'));
-//                   } else {
-//                     _pdfController.loadDocument(PdfDocument.openAsset(
-//                         'assets/pdf/janfie-apresentacao.pdf'));
-//                   }
-//                   isSampleDoc = !isSampleDoc;
-//                 },
-//               )
-//             ],
-//           ),
-//           body: PdfView(
-//             scrollDirection: Axis.vertical,
-//             documentLoader: Center(child: CircularProgressIndicator()),
-//             pageLoader: Center(child: CircularProgressIndicator()),
-//             controller: _pdfController,
-//             onDocumentLoaded: (document) {
-//               setState(() {
-//                 _allPagesCount = document.pagesCount;
-//               });
-//             },
-//             onPageChanged: (page) {
-//               setState(() {
-//                 _actualPageNumber = page;
-//               });
-//             },
-//           ),
-//           floatingActionButton: Row(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             crossAxisAlignment: CrossAxisAlignment.center,
-//             children: [
-//               FloatingActionButton(
-//                 heroTag: 'fab-back',
-//                 child: Icon(Icons.navigate_before, color: Styles.pretao),
-//                 onPressed: () {
-//                   _pdfController.previousPage(
-//                     curve: Curves.linear,
-//                     duration: Duration(milliseconds: 100),
-//                   );
-//                 },
-//               ),
-//               const SizedBox(width: 10),
-//               FloatingActionButton(
-//                 heroTag: 'fab-next',
-//                 child: Icon(Icons.navigate_next, color: Styles.pretao),
-//                 onPressed: () {
-//                   _pdfController.nextPage(
-//                     curve: Curves.linear,
-//                     duration: Duration(milliseconds: 100),
-//                   );
-//                 },
-//               ),
-//             ],
-//           ),
-//         ),
-//       );
-// }
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:lejaum/pages/mobile/services/styles_mobile.dart';
-import 'package:native_pdf_renderer/native_pdf_renderer.dart';
+import 'package:pdf_render/pdf_render_widgets.dart';
 
-class PdfJanfie extends StatelessWidget {
-  Future<PdfDocument> _getDocument() async {
-    if (await hasPdfSupport()) {
-      return PdfDocument.openAsset('assets/pdf/janfie-apresentacao.pdf');
-    }
-
-    throw Exception(
-      'PDF Rendering does not '
-      'support on the system of this version',
-    );
-  }
+class PdfJanfie extends StatefulWidget {
+  const PdfJanfie({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      statusBarBrightness: Brightness.light,
-      statusBarIconBrightness: Brightness.dark,
-      systemNavigationBarColor: Colors.white,
-      systemNavigationBarIconBrightness: Brightness.dark,
-    ));
-
-    final storage = Map<int, PdfPageImage>();
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Styles.pretao,
-        title: Text(
-          'Janfie - Id. Visual e Naming',
-          style: Styles.textoBrancoBold,
-        ),
-      ),
-      body: FutureBuilder(
-        future: _getDocument(),
-        builder: (context, AsyncSnapshot<PdfDocument> snapshot) {
-          if (!snapshot.hasData) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          if (snapshot.hasError) {
-            return Center(
-              child: Text(snapshot.error.toString()),
-            );
-          }
-
-          return ListView.builder(
-            itemCount: 21,
-            itemBuilder: (context, index) {
-              return ImageLoader(
-                key: ValueKey('pdf$index'),
-                storage: storage,
-                document: snapshot.data,
-                pageNumber: index + 1,
-              );
-            },
-          );
-        },
-      ),
-      // bottomNavigationBar: BottomAppBar(
-      //   child: Padding(
-      //     padding: EdgeInsets.all(16),
-      //     child: Row(
-      //       mainAxisSize: MainAxisSize.max,
-      //       mainAxisAlignment: MainAxisAlignment.center,
-      //       children: <Widget>[
-      //         Text(
-      //           'Swipe to right',
-      //           style: Theme.of(context).textTheme.headline6,
-      //         ),
-      //         Icon(Icons.keyboard_arrow_right),
-      //       ],
-      //     ),
-      //   ),
-      // ),
-    );
-  }
+  _PdfJanfieState createState() => _PdfJanfieState();
 }
 
-class ImageLoader extends StatelessWidget {
-  ImageLoader({
-    required this.storage,
-    required this.document,
-    required this.pageNumber,
-    Key? key,
-  }) : super(key: key);
-
-  final Map<int, PdfPageImage?> storage;
-  final PdfDocument? document;
-  final int pageNumber;
-
+class _PdfJanfieState extends State<PdfJanfie> {
   @override
-  Widget build(BuildContext context) => Center(
-        child: FutureBuilder(
-          future: _renderPage(),
-          builder: (context, AsyncSnapshot<PdfPageImage?> snapshot) {
-            if (snapshot.hasError) {
-              return Center(
-                child: Text('Error'),
-              );
-            }
-            if (!snapshot.hasData) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-
-            return Image(
-              image: MemoryImage(snapshot.data!.bytes),
-            );
-          },
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Janfie PDF'),
+      ),
+      backgroundColor: Colors.grey,
+      body: Center(
+        child: PdfDocumentLoader.openAsset(
+          'assets/pdf/janfie-apresentacao.pdf',
+          documentBuilder: (context, pdfDocument, pageCount) => LayoutBuilder(
+            builder: (context, constraints) => ListView.builder(
+              scrollDirection: Axis.vertical,
+              itemCount: pageCount,
+              itemBuilder: (context, index) => Container(
+                margin: EdgeInsets.all(2),
+                padding: EdgeInsets.all(0),
+                color: Colors.black12,
+                child: PdfPageView(
+                  pdfDocument: pdfDocument,
+                  pageNumber: index + 1,
+                ),
+              ),
+            ),
+          ),
         ),
-      );
-
-  Future<PdfPageImage?> _renderPage() async {
-    if (storage.containsKey(pageNumber)) {
-      return storage[pageNumber];
-    }
-    final page = await document!.getPage(pageNumber);
-    final pageImage = await page.render(
-      width: page.width * 2,
-      height: page.height * 2,
-      format: PdfPageFormat.PNG,
+      ),
     );
-    await page.close();
-    storage[pageNumber] = pageImage;
-    return pageImage;
   }
 }
